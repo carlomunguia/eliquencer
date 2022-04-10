f60dfe6bd3e622ce00649f5c0cd544af36c27978
Carlo Munguia <cmunguia@denimgroup.com>
2022-04-10 12:56:30 -0500

    Eliquencer Server 2
    created a supervised server name stow that jumps into action when original server crashes, refactored server.ex for this use, updated application to use rest_for_one vs. one_for_one

--- eliquencer/lib/eliquencer/application.ex
+++ eliquencer/lib/eliquencer/application.ex
@@ -8,12 +8,13 @@ defmodule Eliquencer.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      {Eliquencer.Server, 123},
+      {Eliquencer.Stow, 123},
+      {Eliquencer.Server, nil},
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Eliquencer.Supervisor]
+    opts = [strategy: :rest_for_one, name: Eliquencer.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
--- eliquencer/lib/eliquencer/server.ex
+++ eliquencer/lib/eliquencer/server.ex
@@ -1,6 +1,6 @@
 defmodule Eliquencer.Server do
-  def start_link(current_number) do
-    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
   def next_number do
@@ -11,8 +11,8 @@ defmodule Eliquencer.Server do
     GenServer.cast(__MODULE__, {:increment_number, delta})
   end
 
-  def init(initial_number) do
-    {:ok, initial_number}
+  def init(_) do
+    {:ok, Eliquencer.Stow.get()}
   end
 
   def handle_call(:next_number, _from, current_number) do
@@ -23,9 +23,10 @@ defmodule Eliquencer.Server do
     {:noreply, current_number + delta}
   end
 
-  def format_status(_reason, [_pdict, state]) do
-    [data: [{'State', "My current state is '#{inspect(state)}', and I'm ecstatic!"}]]
-  end
+  def terminate(_reason, current_number) do
+    Eliquencer.Stow.update(current_number)
+  end  
+
   def child_spec(opts) do
       %{
         id: __MODULE__,
